333538f8db5f74f56b17c044190738502b427064
Kayra <KayraAlat@gmail.com>
2020-08-11 09:01:00 +0100

    Added initial books service and get endpoints

--- bookstore-nest/src/books/books.controller.ts
+++ bookstore-nest/src/books/books.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { BooksService } from './books.service';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BooksService) { }
+
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
+
+  @Get(':bookId')
+  async getBook(@Param('bookId') bookId: number) {
+    const book = await this.booksService.getBook(bookId);
+    return book;
+  }
+}
---
+++ bookstore-nest/src/books/books.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import { BOOKS } from '../data/db';
+
+@Injectable()
+export class BooksService {
+  books = BOOKS;
+
+  getBooks(): Promise<any> {
+    return new Promise((resolve) => {
+      resolve(this.books);
+    });
+  }
+
+  getBook(bookId: number): Promise<any> {
+    console.log('hit');
+    const id = Number(bookId);
+    return new Promise((resolve) => {
+      const book = this.books.find((book) => book.id === id);
+      if (!book) {
+        throw new HttpException('Book not found.', 404);
+      }
+
+      resolve(book);
+    });
+  }
+}
